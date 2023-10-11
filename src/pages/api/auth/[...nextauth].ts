9551bffcc6b2c36a1a304b338f8c94cdff18cd63
Niklas Haiden <niklas.haiden@gmail.com>
2023-10-11 11:36:06 +0200

    sync changes

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import KeycloakProvider from "next-auth/providers/keycloak"
+import NextAuth from "next-auth";
+export const authOptions = {
+    providers: [
+        KeycloakProvider({
+            clientId: process.env.KEYCLOAK_CLIENT_ID || "",
+            clientSecret: process.env.KEYCLOAK_CLIENT_SECRET || "",
+            issuer: process.env.KEYCLOAK_ISSUER
+        })
+    ]
+}
+
+export default NextAuth(authOptions)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import {redirect} from "next/navigation";
+import {signIn, useSession} from "next-auth/react";
+import {router} from "next/client";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    const { data: session } = useSession()
+
+    if (session) {
+        router.push("/whew")
+    }
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        className={'flex'}
     >
+        <div
+            className={"flex justify-center items-center flex-col bg-welcomebg bg-no-repeat bg-cover bg-fixed bg-center backdrop-blur-sm bg-white/30 w-1/2 h-screen max-h-[50%]"}>
+            <div className={"drop-shadow-2xl items-center text-white font-black text-8xl"}>Welcome to Aries.</div>
+            <div className={"drop-shadow-2xl text-white font-black text-4xl"}>Deploy away!</div>
+        </div>
 
+        <div className={"flex justify-center items-center flex-col w-1/2 h-screen"}>
+
+            <button onClick={() => signIn('keycloak')}
+                    className={"drop-shadow-2xl text-blue-500 bg-white border transition hover:transition hover:transform duration-500 hover:border-0 border-blue-500 p-4 rounded-2xl text-2xl w-1/2"}>Login</button>
+
+        </div>
     </main>
   )
 }
